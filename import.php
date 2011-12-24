5319c2a3d9389170ae72f1b5717944d6cf5e75a9
Ganesh Katrapati <ganesh@localhost.localdomain>
2011-12-24 18:44:59 +0530

    XYZ

--- import.php
+++ import.php
@@ -11,8 +11,8 @@ if(!array_key_exists('dbname', $keys) || !array_key_exists('dbpass', $keys)
 $dbname = $keys["dbname"][1];
 $dbuser = $keys["dbuser"][1];
 $dbpass = $keys["dbpass"][1];
-$con = mysql_connect("localhost",$dbuser,$dbpass);
-mysql_select_db($dbname, $con);
+$conx = mysql_connect("localhost",$dbuser,$dbpass);
+mysql_select_db($dbname, $conx);
 
 ?>
 <fieldset>
@@ -27,7 +27,7 @@ mysql_select_db($dbname, $con);
 			echo "<div class='box' style='width:60%'>";
 			echo  "<h3 class='label success'>Available Results</h3>";
 			echo "<form action='#' class='uniForm' method='post'>";
-			$q = mysql_query("select * from MRESULTT");
+			$q = mysql_query("select * from MRESULTT",$conx);
 			echo "<table class='bttable'>";
 			while($row = mysql_fetch_array($q))
 			{
@@ -39,10 +39,65 @@ mysql_select_db($dbname, $con);
 			}
 		}
 		else{
+			$result = mysql_query("select * from ".$_GET['tabid']);
+			echo "<form action='' method='post'>";
+			echo "<h3 class='label success'>These Following Student Records Were Recognized Submit To Import <input type='submit' style='height:20%' class='btn primary' value='Submit'></input></h3>";
+			echo "";
+			echo "<table class='bttable'>";
+			echo "<th class='zebra-striped' colspan='2'>Name</th>";
+			echo "<th class='zebra-striped'>Subject</th>";
+			echo "<th class='zebra-striped'>Internals</th>";
+			echo "<th class='zebra-striped'>Externals</th>";
+			echo "<th class='zebra-striped'>Credits</th>";
+			while ($row = mysql_fetch_array($result)) {
+				
 			
+			
+				$x = lookupStudent($row['srno']);
+				if($x != "false"){
+					echo "<tr>";
+					$name = $x['sname'];
+					$sid =  $x['sid'];
+					$subject = $row["subname"];
+					$intm = $row["intm"];
+					$extm = $row["extm"];
+					$cre = $row["cre"];
+					$imgurl = getImageURL($x['imgid']);
+					echo "<td><img src='../$imgurl' width='50' height='50'></img></td>";
+					echo "<td>$name</td>";
+					echo "<td>$subject</td>";
+					echo "<td>$intm</td>";
+					echo "<td>$extm</td>";
+					echo "<td>$cre</td>";
+					echo "</tr>";
+				}
+				
+			}
+			echo "</table>";
+			echo "</form>";
 		}
 		?>
 			</div>
 		</center>
+		<?php 
+			function lookupStudent($srno){
+				//echo "select * from freeEdu.MSTUDENTT s where s.srno like '$srno' ";
+				$res = mysql_query("select * from freeEdu.MSTUDENTT s where s.srno like '$srno' ");
+				if(mysql_num_rows($res)<=0)
+					return "false";
+				return mysql_fetch_array($res);
+			}
+			function getImageURL($imgid){
+				//echo "select * from freeEdu.MSTUDENTT s where s.srno like '$srno' ";
+				$res = mysql_query("select * from freeEdu.MIMGT s where s.imgid like '$imgid' ");
+				if(mysql_num_rows($res)<=0 )
+				return "images/faces/student.png";
+				$y =  mysql_fetch_array($res);
+				if(!file_exists("../".$y['imguri'])){
+					return "images/faces/student.png";
+				}
+				return $y['imguri'];
+			}
+		?>
 	</body>
 </fieldset>
